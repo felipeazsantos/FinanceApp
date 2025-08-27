0ea5caea8735df671b520fc6f5e494323b736bea
felipeazsantos <felipeasantos10@hotmail.com>
2025-08-27 15:44:10 -0300

    project structure created

---
+++ app/src/main/java/dev/felipeazsantos/financeapp/MainActivity.kt
@@ -0,0 +1,52 @@
+package dev.felipeazsantos.financeapp
+
+import android.R
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.felipeazsantos.financeapp.ui.theme.FinanceAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FinanceAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(modifier: Modifier) {
+    Surface(
+        modifier = modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Text("Starting project")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FinanceAppPreview() {
+    FinanceAppTheme {
+        MainScreen(
+            modifier = Modifier.padding(top = 20.dp)
+        )
+    }
+}
